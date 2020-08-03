aae0c7e4310d2060c07e87376f1c9a222956f5e8
wuyaheng <wuyaheng2016@gmail.com>
2020-08-03 15:07:43 -0400

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import { Cards, Chart, CountryPicker } from './components';
+import { fetchData } from './API'
 
 import styles from './App.module.css';
 
 class App extends Component {
+
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data)
+  }
   render() {
     return (
       <div className={styles.container}> 
